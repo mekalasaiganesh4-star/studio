d430bf7c5be7b926c3a5dc82860fb4816a166d3e
Mekala sai ganesh <mekalasaiganesh4@gmail.com>
2026-02-19 04:19:21 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2957402532-b0635",
+  "appId": "1:918347304746:web:cda64e0da24fee84069853",
+  "apiKey": "AIzaSyB4EeApldKNeBHJCDzc-BukZNId_FZbjto",
+  "authDomain": "studio-2957402532-b0635.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "918347304746"
+};
